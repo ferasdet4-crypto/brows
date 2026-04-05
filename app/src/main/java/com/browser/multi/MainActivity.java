b5223ca758c661a0b6b15229838c9014c22ac6b0
ferasdet4-crypto <ferasdet4@gmail.com>
2026-04-05 10:43:47 +0300

    Upload app/src/main/java/com/browser/multi/MainActivity.java

---
+++ app/src/main/java/com/browser/multi/MainActivity.java
@@ -0,0 +1,23 @@
+package com.browser.multi;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView wv = new WebView(this);
+        setContentView(wv);
+        WebSettings s = wv.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        CookieManager.getInstance().removeAllCookies(null);
+        CookieManager.getInstance().flush();
+        wv.setWebViewClient(new WebViewClient());
+        wv.loadUrl("https://whoer.net");
+    }
+}
