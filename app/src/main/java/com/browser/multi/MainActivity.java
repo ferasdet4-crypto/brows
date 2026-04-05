28f64c63f6b87ad843cdd6c04f62e1956084e48d
ferasdet4-crypto <ferasdet4@gmail.com>
2026-04-05 10:58:59 +0300

    Upload app/src/main/java/com/browser/multi/MainActivity.java

--- app/src/main/java/com/browser/multi/MainActivity.java
+++ app/src/main/java/com/browser/multi/MainActivity.java
@@ -5,25 +5,17 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         WebView wv = new WebView(this);
         setContentView(wv);
-        
         WebSettings s = wv.getSettings();
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
-        s.setDatabaseEnabled(true);
-        
-        // Ключевой момент для "каждая вкладка - новая сессия"
-        // Очищаем куки и кэш при каждом запуске приложения
         CookieManager.getInstance().removeAllCookies(null);
         CookieManager.getInstance().flush();
-        wv.clearCache(true);
-        
         wv.setWebViewClient(new WebViewClient());
         wv.loadUrl("https://whoer.net");
     }
